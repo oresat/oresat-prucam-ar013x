4f049ce95afec11e7db77dc159089279f5a6712e
ryanpdx <rmedick@pdx.edu>
2020-09-05 17:20:58 -0700

    finished package req. added examples and quick readme

--- python-prucam/setup.py
+++ python-prucam/setup.py
@@ -5,4 +5,8 @@ import sys
 import setuptools
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
-setuptools.setup()
+setuptools.setup(
+    package_data={
+        'prucam': ['fw/*.out'],
+        },
+    )
