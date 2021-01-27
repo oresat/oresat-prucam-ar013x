0c05dc7fdfb0c17829f2764b5711fe2a8d50de00
ryanpdx <rmedick@pdx.edu>
2021-01-27 12:10:09 -0800

    logic cleanup for pru.py and rework for pytests

--- src/python3_lib/tests/test_pru.py
+++ src/python3_lib/tests/test_pru.py
@@ -1,63 +1,44 @@
 """Unittest for PRU class"""
 
+import pytest
 from prucam import PRUCam
 
 pru = PRUCam()
 
 
-def test_start():
+@pytest.fixture
+def prucam():
+    """make the PRUCam obj and  test loading the pru firmware onto the pru"""
+
+    return PRUCam()
+
+
+def test_start(prucam):
     """test staring the pru"""
-    try:
-        pru.start()
-    except Exception as exc:
-        print(exc)
-        assert False
+
+    prucam.start()
     assert True
 
 
-def test_stop():
+def test_stop(prucam):
     """test stoping the pru"""
-    try:
-        pru.stop()
-    except Exception as exc:
-        print(exc)
-        assert False
+
+    prucam.stop()
     assert True
 
 
-def test_restart():
+def test_restart(prucam):
     """test restarting the pru"""
 
     # turn pru back on for test
-    pru.start()
+    prucam.start()
 
-    try:
-        pru.restart()
-    except Exception as exc:
-        print(exc)
-        assert False
+    prucam.restart()
     assert True
 
 
-def test_state():
+def test_status(prucam):
     """test getting the state of the pru"""
-    try:
-        pru.state()
-    except Exception as exc:
-        print(exc)
-        assert False
+
+    prucam.status()
     assert True
-
-
-def test_load_fw():
-    """test loading the pru firmware onto the pru"""
-
-    pru.stop()
-
-    try:
-        pru._load_fw()
-    except Exception as exc:
-        print(exc)
-        assert False
-    assert True
-
