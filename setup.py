c224f8616725ace5e7ae912eade610099fc8ccc6
ryanpdx <rmedick@pdx.edu>
2021-04-08 10:42:38 -0700

    Fixed build bugs for python3-prucam debian package
    - moved setup.py to / so debian/rules can find it
    - added debian/python3-prucam.install so the src code is added to the
      package
    - setup.py no longer imports prucam module

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 """ OreSat Linux PRUcam library Setup.py """
 
 from setuptools import setup
-import prucam
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name=prucam.APP_NAME,
-    version=prucam.APP_VERSION,
-    author=prucam.APP_AUTHOR,
-    license=prucam.APP_LICENSE,
-    description=prucam.APP_DESCRIPTION,
+    name="prucam",
+    version="0.3.2",
+    author="PSAS",
+    license="GPL-3.0",
+    description="A python library for interfacing the prucam kernel module",
     long_description=long_description,
-    author_email="rmedick@pdx.edu",
-    maintainer="Ryan Medick",
-    maintainer_email="rmedick@pdx.edu",
+    maintainer="PSAS",
+    maintainer_email="oresat@pdx.edu",
     url="https://github.com/oresat/oresat-linux-prucam",
     packages=['prucam'],
+    package_dir={'prucam': 'src/python3_lib/prucam'},
     install_requires=[
         "opencv-python",
     ],
--- src/python3_lib/prucam/__init__.py
+++ src/python3_lib/prucam/__init__.py
@@ -3,14 +3,3 @@
 from .camera import Camera, CONTEXT_A, CONTEXT_B
 from .pru import PRUCam
 
-MAJOR = 0
-MINOR = 3
-PATCH = 0
-
-APP_NAME = "prucam"
-APP_DESCRIPTION = "A python library for interfacing the prucam kernel module"
-APP_VERSION = "{}.{}.{}".format(MAJOR, MINOR, PATCH)
-APP_AUTHOR = "Ryan Medick"
-APP_EMAIL = "rmedick@pdx.edu"
-APP_URL = "https://github.com/oresat/oresat-linux-updater"
-APP_LICENSE = "GPL-3.0"
