f959254f88395888bad276d2ce1c8c1b659d69ec
Oliver Rew <emailoliverrew@gmail.com>
2022-08-19 16:09:02 -0400

    fix: set PRU FW file name in deploy.sh

--- src/pru_code/deploy.sh
+++ src/pru_code/deploy.sh
@@ -11,11 +11,17 @@ echo "-Placing the firmware"
 	sudo cp gen/prucam_pru0_fw.out /lib/firmware/
 	sudo cp gen/prucam_pru1_fw.out /lib/firmware/
 
+echo "Setting PRU Firmware Name"
+sudo su -c 'echo "prucam_pru0_fw.out" > /sys/class/remoteproc/remoteproc1/firmware'
+sudo su -c 'echo "prucam_pru1_fw.out" > /sys/class/remoteproc/remoteproc2/firmware'
+sleep 0.1s
+
 echo "-Rebooting"
 echo "Rebooting pru-core 1"
 sudo su -c 'echo "stop" > /sys/class/remoteproc/remoteproc2/state'
 sudo su -c 'echo "start" > /sys/class/remoteproc/remoteproc2/state'
 sleep 0.1s
+
 echo "Rebooting pru-core 0"
 sudo su -c 'echo "stop" > /sys/class/remoteproc/remoteproc1/state'
 sudo su -c 'echo "start" > /sys/class/remoteproc/remoteproc1/state'
